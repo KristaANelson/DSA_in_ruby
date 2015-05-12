f3eda2f402a0e2d064e2801ebd7b9b9d938ec510
KristaANelson <krista.a.nelson@gmail.com>
2015-05-12 16:52:45 -0600

    reverse words

---
+++ Chapter1_Array-String/6_reverse_words_string_spec.rb
@@ -0,0 +1,58 @@
+# Given an input string s, reverse the string word by word.
+# For example, given s = "the sky is blue", return "blue is sky the".
+
+class WordReverser
+  def reverse_words(string)
+    string.split(" ").reverse.join(" ")
+  end
+
+  def reverse_words_without_using_reverse_or_split(string)
+    word = ""
+    tracker = []
+    string.chars.each do |char|
+      if char != " "
+        word += char
+      else
+        tracker.unshift(word) if word != ""
+        word = ""
+      end
+    end
+    tracker.unshift(word) if word != ""
+    tracker.join(" ")
+  end
+end
+require 'rspec'
+
+RSpec.describe WordReverser do
+  it 'can reverse a string word by word' do
+    wr = WordReverser.new
+
+    answer = wr.reverse_words("the sky is blue")
+
+    expect(answer).to eq("blue is sky the")
+  end
+
+  it 'can reverse a string with double word by word' do
+    wr = WordReverser.new
+
+    answer = wr.reverse_words("the  sky is blue")
+
+    expect(answer).to eq("blue is sky the")
+  end
+
+  it 'can reverse a string with double word by word without using reverse or split' do
+    wr = WordReverser.new
+
+    answer = wr.reverse_words_without_using_reverse_or_split("the sky is blue")
+
+    expect(answer).to eq("blue is sky the")
+  end
+
+  it 'can reverse a string with double word by word without using reverse or split' do
+    wr = WordReverser.new
+
+    answer = wr.reverse_words_without_using_reverse_or_split("the sky is  blue")
+
+    expect(answer).to eq("blue is sky the")
+  end
+end
