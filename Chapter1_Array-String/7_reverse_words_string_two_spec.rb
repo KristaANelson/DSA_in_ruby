21ad50df33dd44dcddd5cafd2e19f7e8e5ce12fd
KristaANelson <krista.a.nelson@gmail.com>
2015-05-12 18:00:44 -0600

    reverse another way

--- Chapter1_Array-String/7_reverse_words_string_two_spec.rb
+++ Chapter1_Array-String/7_reverse_words_string_two_spec.rb
@@ -7,12 +7,24 @@ class WordReverserTwo
         i, j = index
       elsif string[index] != " " && i != ""
         j = index
-      else 
+      else
         string[i..j] = string[i..j].reverse
         i, j = ""
       end
     end
     string[i..j] = string[i..j].reverse
+    string
+  end
+
+  def reverse_another_way(string)
+    string = string.reverse
+    i = 0
+    (string.length + 1).times do |j|
+      if j == (string.length) || string[j] == " "
+        string[i..j-1] = string[i..j-1].reverse
+        i = j + 1
+      end
+    end
     string
   end
 end
@@ -23,6 +35,12 @@ RSpec.describe WordReverserTwo do
   it 'can reverse words in place' do
     wrt = WordReverserTwo.new
     answer = wrt.reverse_words("the sky is blue")
+    expect(answer).to eq("blue is sky the")
+  end
+
+  it 'can reverse words in place another way' do
+    wrt = WordReverserTwo.new
+    answer = wrt.reverse_another_way("the sky is blue")
     expect(answer).to eq("blue is sky the")
   end
 end
